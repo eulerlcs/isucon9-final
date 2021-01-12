603e2044ebbdc4dc202dd4ea9ba5474cabfc908f
user01 <liu-chunsheng@zhimingsoft.jp>
2021-01-12 14:47:47 +0900

    reset mybatis config

--- webapp/java/src/main/java/jp/zhimingsoft/www/isucon/configuration/AppConfiguration.java
+++ webapp/java/src/main/java/jp/zhimingsoft/www/isucon/configuration/AppConfiguration.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.logging.slf4j.Slf4jImpl;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +18,7 @@ import javax.sql.DataSource;
 
 @Configuration
 @Slf4j
+@MapperScan(basePackages = {"jp.zhimingsoft.www.isucon.dao"})
 public class AppConfiguration {
 
     @Bean
@@ -25,40 +27,22 @@ public class AppConfiguration {
         return restTemplateBuilder.messageConverters(new MappingJackson2HttpMessageConverter(objectMapper)).build();
     }
 
-//    @Autowired
-//    private DataSource dataSource;
-
-//    @Bean(name = "dataSource")
-//    @ConfigurationProperties(prefix = "spring.datasource")
-//    public DataSource dataSource() {
-//        return DataSourceBuilder.create().build();
-//    }
-
-    @Bean("sqlSessionFactoryBean")
-    public SqlSessionFactory getSqlSessionFactory(DataSource dataSource) throws Exception {
-        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-        bean.setDataSource(dataSource);
-        SqlSessionFactory factory = bean.getObject();
-        org.apache.ibatis.session.Configuration configuration = factory.getConfiguration();
-        configuration.setLogImpl(Slf4jImpl.class);
-        configuration.setMapUnderscoreToCamelCase(true);
-        return factory;
-    }
-
-//    @Bean
-//    public org.apache.ibatis.session.Configuration configuration() {
-//        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+//    @Bean("sqlSessionFactoryBean")
+//    public SqlSessionFactory getSqlSessionFactory(DataSource dataSource) throws Exception {
+//        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+//        bean.setDataSource(dataSource);
+//        SqlSessionFactory factory = bean.getObject();
+//        org.apache.ibatis.session.Configuration configuration = factory.getConfiguration();
 //        configuration.setLogImpl(Slf4jImpl.class);
 //        configuration.setMapUnderscoreToCamelCase(true);
-//        //configuration.setDefaultSqlProviderType(TemplateFilePathProvider.class);
-//        return configuration;
+//        return factory;
 //    }
-
-    @Bean
-    public MapperScannerConfigurer mapperScannerConfigurer() {
-        MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
-        mapperScannerConfigurer.setBasePackage(Marker.class.getPackageName());
-        mapperScannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactoryBean");
-        return mapperScannerConfigurer;
-    }
+//
+//    @Bean
+//    public MapperScannerConfigurer mapperScannerConfigurer() {
+//        MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
+//        mapperScannerConfigurer.setBasePackage(Marker.class.getPackageName());
+//        mapperScannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactoryBean");
+//        return mapperScannerConfigurer;
+//    }
 }
